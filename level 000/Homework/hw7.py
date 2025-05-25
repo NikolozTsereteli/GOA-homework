6cb361c39098ba431c06bf078d22bb1e0498a5c2
NikolozTsereteli <nikotsereteli12@gmail.com>
2025-05-25 21:11:32 +0400

    commit

---
+++ level 000/Homework/hw7.py
@@ -0,0 +1,6 @@
+# 7) მომხმარებელს შემოატანინე რამე ტექსტი და ეს ტექსტი გამოიტანეთ 10-ჯერ
+
+text = input("enter some text please: ")
+for i in range(10):
+    print(text)
+    
---
+++ level 000/Homework/hw8.py
@@ -0,0 +1,6 @@
+# 8) მომხმარებელს შემოატანინე სახელი, ასაკი, სიმაღლე და დაპრინტეთ ეგ ყველაფერი ერთ დიდ წინადადებად. (როდესაც მომხმარებელი შემოიტანს იქვე გადააქციეთ სხვადასხვა მონაცემთა ტიპად. ანუ ასაკი int-ად)
+
+name = input("Your name is: ")
+age = int(input("Your age is: "))
+height = float(input("Your height is:"))
+print("My name is " + name + " and I am " + str(age) + " years old, and I am " + str(height) + " meters tall.")
