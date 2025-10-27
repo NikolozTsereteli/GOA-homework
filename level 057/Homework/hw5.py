010c5d3e33ccac8ad120d404130af80e94dc93e6
NikolozTsereteli <nikotsereteli12@gmail.com>
2025-10-27 16:12:03 +0400

    commit

--- level 057/Homework/hw5.py
+++ level 057/Homework/hw5.py
@@ -2,12 +2,6 @@ def first_non_consecutive(arr):
     for i in range(0, len(arr[:-1])):
         if arr[i] + 1 != arr[i + 1]:
             return arr[i + 1]
-
-
-
-listt = ["apple", "banana", "peach", "cherry", "strawberry"]
-
-for i in range(5):
     
 
 
